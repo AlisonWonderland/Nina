2c984d5229787e2fd189e758b2b624637edaa38a
AlisonWonderland <dimitridawn@gmail.com>
2019-09-10 19:45:03 -0700

    Reddit api request can be done now

--- README.md
+++ README.md
@@ -5,12 +5,16 @@
 <!-- * Add submit button for text box -->
 <!-- * Send text through the inputs and see if you get right output. use send. STILL NEED
 TEXTAREA INPUT -->
-* Get tweet data
+<!-- * Get tweet data -->
 * Get reddit data
 * Create new ejs page to load this data(prefered) or add loading animation.
 * Find ibm node package, use it.
 * Styles. *** USE LANDING PAGE CSS FILE FOR THE DATA PAGE ***
 * Responsiveness
+
+
+### Refactoring ideas.
+Put twitter and reddit api request functions in their own js files in 'public' like Ninas-insights.
 
 ## Acroynms I used
 'ws' means website
--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-var express = require('express');
-var app = express();
-var fetch = require('node-fetch');
-var Twitter = require('twitter');
-
-// File with API keys in it
-var config = require('./config.js');
+const express = require('express');
+const app = express();
+const fetch = require('node-fetch');
+const Twitter = require('twitter');
+const snoowrap = require('snoowrap');
+const PersonalityInsightsV3 = require('ibm-watson/personality-insights/v3');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(__dirname + '/public/html'));
@@ -12,36 +11,96 @@ app.use(express.static(__dirname + '/public/css'));
 app.use(express.static(__dirname + '/public/js'));
 app.set("view engine", "ejs");
 
+// File with API keys in it
+const config = require('./config.js');
 
-var client = new Twitter(config);
 
-client.get('statuses/user_timeline', {tweet_mode: 'extended', screen_name: 'realDonaldTrump',count: 5, include_rts: false},function(error, tweets, response) {
-    if(error) throw error;
-    console.log(tweets[0].full_text);  // The favorites.
-});
-
+// All routes work so far
 app.get('/', function(req, res) { 
     res.render('landingPage');
 });
 
 app.post('/reddit-search', function(req, res) {
-    var username = req.body.user_search;
+    const username = req.body.user_search;
     // makeRedditRequest(username);
     res.send(req.body.user_search);
     // res.redirect('/');
 })
 
 app.post('/twitter-search', function(req, res) {
-    var username = req.body.user_search;
-    // makeTwitterRequest(username);
-    res.send(req.body.user_search);
+    const username = req.body.user_search;
+    makeTwitterRequest(username);
+    res.redirect('/');
 })
 
 app.post('/personal-text', function(req, res) {
-    var usertext = req.body.usertext;
-    console.log("hi");
+    const usertext = req.body.usertext;
     res.send();
 });
+
+
+// ******* HELPER FUNCTIONS **********
+
+// Twitter related functions
+var twitterClient = new Twitter(config.twitterCred);
+function makeTwitterRequest(username) {
+    var tweetParams = {
+        tweet_mode: 'extended', 
+        screen_name: username,
+        count: 5, 
+        include_rts: false
+    };
+
+    twitterClient.get('statuses/user_timeline', tweetParams, function(error, tweets, response) {
+        if(error) throw error;
+        for(let i = 0; i < tweets.length; ++i) {
+            console.log(tweets[i].full_text);  // The favorites.
+        }
+    });
+}
+
+// Reddit related functions
+const REDDIT_ACCESS_TOKEN_URL = 'https://www.reddit.com/api/v1/access_token';
+
+
+async function createSnoowrap() {
+    var tokenData = await getRedditAccessToken();
+    
+    const r = new snoowrap({
+        userAgent: 'Nina',
+        accessToken: tokenData.access_token
+    });
+
+    r.getUser('yz50Racer').getComments()[0].then(console.log);
+}
+
+async function makeRedditRequest(username) {
+    console.log("requesting token");
+    var access_token = await getRedditAccessToken();
+    console.log(access_token);
+}
+
+async function getRedditAccessToken() {
+    const  REDDIT_CLIENT_ID  = 'ihUtacXSBGpYfg';
+    const REDDIT_CLIENT_SECRET = 'pE96_1ULKQ80GVsYmyj59z4uAcU';
+
+    const tokenData = await fetch(REDDIT_ACCESS_TOKEN_URL, {
+        method: 'POST',
+        body: 'grant_type=client_credentials',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': `Basic ` + Buffer.from(`${REDDIT_CLIENT_ID}:${REDDIT_CLIENT_SECRET}`).toString('base64') // Put password as empty
+        }
+    }).then(function(response) {
+        return response.json();
+    })
+
+    // Could return just tokenData.access_token. But, this is useful for checking expiration.
+    return tokenData;
+}
+
+// Ibm related functions
+const personalityInsights = new PersonalityInsightsV3(config.ibmCred);
 
 // Start the server on port 8080
 app.listen(8080, function(req, res) {
